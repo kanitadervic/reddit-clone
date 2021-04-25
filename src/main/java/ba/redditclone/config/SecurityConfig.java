a781d8c976fe6f4ad2fd203ef4513bf7590e134f
kdervic1 <kdervic1@etf.unsa.ba>
2021-04-26 00:00:44 +0200

    added password encoder

--- src/main/java/ba/redditclone/config/SecurityConfig.java
+++ src/main/java/ba/redditclone/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package ba.redditclone.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,5 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ba/redditclone/service/AuthService.java
+++ src/main/java/ba/redditclone/service/AuthService.java
@@ -2,19 +2,31 @@ package ba.redditclone.service;
 
 import ba.redditclone.http.request.RegisterRequest;
 import ba.redditclone.model.User;
+import ba.redditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user= new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
     }
 }
