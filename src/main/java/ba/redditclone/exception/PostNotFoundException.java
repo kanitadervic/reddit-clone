262eaad903f7b0d23b3c53c41b955460c9aaaecf
kdervic1 <kdervic1@etf.unsa.ba>
2021-05-04 12:14:07 +0200

    added frontend

--- src/main/java/ba/redditclone/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ba.redditclone.exception;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ba/redditclone/exception/SubredditNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ba.redditclone.exception;
-
-public class SubredditNotFoundException extends RuntimeException {
-    public SubredditNotFoundException(String message) {
-        super(message);
-    }
-}
