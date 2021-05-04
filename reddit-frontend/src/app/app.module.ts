eef68c51021ab8304a6d3f09c5d98e71ff621c2b
kdervic1 <kdervic1@etf.unsa.ba>
2021-05-04 13:35:45 +0200

    added header

--- reddit-frontend/src/app/app.module.ts
+++ reddit-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MaterialModule} from "./material/material.module";
+// import {BrowserAnimationsModule} from '@angular/platform browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    // BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
